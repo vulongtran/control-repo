55e100fb882e74572882462a559eadedb53c0623
vulongtran <vulongtran@gmail.com>
2020-11-26 14:16:44 +0800

    commit ntpmodule comments

--- site-modules/profile/manifests/ntpmodule.pp
+++ site-modules/profile/manifests/ntpmodule.pp
@@ -3,3 +3,6 @@ class profile::ntpmodule {
       servers => [ 'time3.google.com', 'time4.google.com'],
 }
 }
+# This is using resouce like class. That is:
+# Resource-like declarations
+# https://puppet.com/docs/puppet/6.19/lang_classes.html#:~:text=Resource%2Dlike%20class%20declarations%20require,to%20avoid%20conflicting%20parameter%20values.
